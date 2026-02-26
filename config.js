e59f7a2f269c78c31e2ad86d939c8cbc9a46c936
devy <202538080154@zjnu.edu.cn>
2026-02-27 00:10:23 +0800

    chore: set stable vercel api base

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.SUPABASE_URL = "https://ypklipqkngswhyifuoyx.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_u7SRFFezpCGOl9czulWkrg_pPrVnSyh";
-window.API_BASE = "https://question-site-l48auqpny-combodevys-projects.vercel.app";
+window.API_BASE = "https://question-site-lac.vercel.app";
