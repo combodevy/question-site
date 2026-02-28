f373ec72bf5c8a394eb94a39aa0ceb557d2596c8
devy <202538080154@zjnu.edu.cn>
2026-02-28 20:05:06 +0800

    fix: prevent history data loss in incremental loadFromCloud + fix CORS for ETag

--- api/_cors.js
+++ api/_cors.js
@@ -14,7 +14,8 @@ function handleCors(req, res) {
     const origin = process.env.CORS_ORIGIN || '*';
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, If-None-Match');
+    res.setHeader('Access-Control-Expose-Headers', 'ETag');
     res.setHeader('Access-Control-Max-Age', '86400');
     if (req.method === 'OPTIONS') {
         res.status(204).end();
